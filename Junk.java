626e467d8b6a772d339ee4400f6e82ac5b62d0af
Dr. Ray Klump <klumpra@lewisu.edu>
2019-10-19 13:17:01 -0500

    hey

--- Junk.java
+++ Junk.java
@@ -2,5 +2,6 @@ public class Junk {
 	public static void main(String[] args) {
 		System.out.println("Hey there, Mojo!");
 		System.out.println("Will this work?");
+		System.out.println("Child");
 	}
 }
